a3c3e27c16672276ce0e01012f23c8715900f051
Simms Labs <simmslabs@gmail.com>
2025-08-10 21:26:38 +0000

    feat: Add Notification model and related enums to the database
    - Created NotificationType, NotificationChannel, and NotificationStatus enums.
    - Implemented Notification table with necessary fields and constraints.
    - Added foreign key relationships to Booking and User tables.
    - Created a sample Excel file for guest import with predefined data.
    - Implemented scripts to check payment data and create sample Excel files.
    - Added a test script for the cron notification API.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from '@vercel/remix/vite';
 
 declare module "@remix-run/node" {
   interface Future {
@@ -9,8 +10,17 @@ declare module "@remix-run/node" {
 }
 
 export default defineConfig({
+  ssr: {
+    external: ["@prisma/client"]
+  },
+  resolve: {
+    alias: {
+      ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+    }
+  },
   plugins: [
     remix({
+      presets: [vercelPreset()],
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
